9b00460311c7889190b6d22f070c3e8f97f41509
sophie_lavender <wjddbwls@ewhain.net>
2024-07-14 23:34:42 +0900

    init: project initiate

---
+++ src/main/java/com/team1/dojang_crush/DojangCrushApplication.java
@@ -0,0 +1,13 @@
+package com.team1.dojang_crush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojangCrushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojangCrushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team1/dojang_crush/DojangCrushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.dojang_crush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojangCrushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
