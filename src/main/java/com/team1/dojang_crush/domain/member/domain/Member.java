8b09cf818ce60ce3a663a109ea3e0d2a1207cb79
seona-moon <fhflwhwl5@gmail.com>
2024-07-16 17:30:50 +0900

    feat: 멤버 엔티티와 레포지토리 생성

---
+++ src/main/java/com/team1/dojang_crush/domain/member/domain/Member.java
@@ -0,0 +1,37 @@
+package com.team1.dojang_crush.domain.member.domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id", updatable = false)
+    private Long memberId;
+
+    @Column(name = "email")
+    @NotBlank
+    private String email;
+
+    @Column(name = "access_token")
+    private String accessToken;
+
+    @Column(name = "refresh_token")
+    private String refreshToken;
+}
---
+++ src/main/java/com/team1/dojang_crush/domain/member/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.team1.dojang_crush.domain.member.repository;
+
+import com.team1.dojang_crush.domain.member.domain.Member;
+import java.util.Optional;
+
+public class MemberRepository {
+
+}
