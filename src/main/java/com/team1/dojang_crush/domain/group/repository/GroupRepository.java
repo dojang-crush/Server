210ab9a4ae7f4010652f811f510d684e1180ad06
hyejin2234 <144921254+hyejin2234@users.noreply.github.com>
2024-07-23 17:51:35 +0900

    feat: group crud 구현

--- src/main/java/com/team1/dojang_crush/domain/group/repository/GroupRepository.java
+++ src/main/java/com/team1/dojang_crush/domain/group/repository/GroupRepository.java
@@ -4,4 +4,5 @@ import com.team1.dojang_crush.domain.group.domain.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
+    Group findByGroupCode(String groupCode);
 }
